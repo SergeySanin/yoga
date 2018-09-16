b2c7c502d9aaf68d44e2b4efd1f686fa11ab1fdd
Sergey Sanin <sergey.s4nin@gmail.com>
2018-09-16 05:54:42 +0300

    Section Shop complet

---
+++ js/javascript.js
@@ -0,0 +1,23 @@
+$("#btn-one").mouseover(function() {
+	$("#header-one").addClass("hover");
+});
+$("#btn-one").mouseout(function() {
+	$("#header-one").removeClass("hover");
+});
+
+
+$("#btn-two").mouseover(function() {
+	$("#header-two").addClass("hover");
+});
+$("#btn-two").mouseout(function() {
+	$("#header-two").removeClass("hover");
+});
+
+
+$("#btn-three").mouseover(function() {
+	$("#header-three").addClass("hover");
+});
+$("#btn-three").mouseout(function() {
+	$("#header-three").removeClass("hover");
+});
+
